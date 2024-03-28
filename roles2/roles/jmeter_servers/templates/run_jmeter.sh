0caf0fddb6a1802196c7155f5c7177230286e483
Anisetti-Jayathi-ML <Anisetti.Jayathi@maplelabs.com>
2024-03-28 15:50:23 +0530

    deleted roles2 folder

--- roles2/roles/jmeter_servers/templates/run_jmeter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Navigate to JMeter bin directory
-cd /opt/apache-jmeter-5.5/bin
-
-# Run JMeter with the specified JMX script
-./jmeter -n -t /home/administrator/petclinic-jmeter.jmx -l /home/administrator/results.jtl
